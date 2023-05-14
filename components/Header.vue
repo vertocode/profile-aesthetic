796f83ba290d654015ccf4a8edbbbc8513efbbee
vertocode <evertonvanoni1@gmail.com>
2023-05-13 21:48:49 -0300

    feat: add a base of the portfolio with about me
    (cherry picked from commit 82009f87cbadb7cd8a935d851da61f8fab51fc3b)

---
+++ components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <header>
+      Érica Esteticista
+    </header>
+    <nav class="menu">
+      <a href="#about-me">Sobre Mim</a>
+      <a href="#services">Serviços</a>
+      <a href="#portfolio">Portfólio</a>
+      <a href="#contact">Contato</a>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+header {
+  background-color: #f99;
+  padding: 20px;
+  text-align: center;
+  color: #fff;
+  font-size: 24px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+}
+.menu {
+  background-color: #eee;
+  padding: 10px;
+  text-align: center;
+}
+.menu a {
+  display: inline-block;
+  padding: 10px;
+  color: #333;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  margin-right: 20px;
+}
+.menu a:hover {
+  color: #f99;
+}
+</style>
+<script setup lang="ts">
+</script>
